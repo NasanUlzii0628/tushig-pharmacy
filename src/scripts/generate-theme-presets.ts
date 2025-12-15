e61fba8270ea01b7e4029cfeb5a49af3a8623402
Dulguun Munkhjargal <dulguun2k1000@gmail.com>
2025-12-16 01:37:01 +0800

    build error fixed

--- src/scripts/generate-theme-presets.ts
+++ src/scripts/generate-theme-presets.ts
@@ -16,8 +16,13 @@
  */
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import prettier from "prettier";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const presetDir = path.resolve(__dirname, "../styles/presets");
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,5 @@
-export * from "./auth";
 export * from "./global";
 export * from "./menu";
 export * from "./role";
-export * from "./user";
 export * from "./product";
 
